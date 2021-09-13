d1b867db35533a7e36dbd17edde4b49f1d0b546b
Jordan Knott <jordan@jordanthedev.com>
2021-09-13 12:43:39 -0500

    deps: upgrade @types/react & @types/react-dom

--- frontend/src/shared/components/ProfileIcon/index.tsx
+++ frontend/src/shared/components/ProfileIcon/index.tsx
@@ -2,15 +2,15 @@ import React, { useRef } from 'react';
 import styled from 'styled-components';
 
 export const Container = styled.div<{ size: number | string; bgColor: string | null; backgroundURL: string | null }>`
-  width: ${props => props.size}px;
-  height: ${props => props.size}px;
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
   border-radius: 9999px;
   display: flex;
   align-items: center;
   justify-content: center;
   color: #fff;
   font-weight: 700;
-  background: ${props => (props.backgroundURL ? `url(${props.backgroundURL})` : props.bgColor)};
+  background: ${(props) => (props.backgroundURL ? `url(${props.backgroundURL})` : props.bgColor)};
   background-position: center;
   background-size: contain;
 `;
@@ -22,6 +22,10 @@ type ProfileIconProps = {
 };
 
 const ProfileIcon: React.FC<ProfileIconProps> = ({ user, onProfileClick, size }) => {
+  let realSize = size;
+  if (size === null) {
+    realSize = 28;
+  }
   const $profileRef = useRef<HTMLDivElement>(null);
   return (
     <Container
@@ -29,17 +33,13 @@ const ProfileIcon: React.FC<ProfileIconProps> = ({ user, onProfileClick, size })
       onClick={() => {
         onProfileClick($profileRef, user);
       }}
-      size={size}
+      size={realSize}
       backgroundURL={user.profileIcon.url ?? null}
       bgColor={user.profileIcon.bgColor ?? null}
     >
       {(!user.profileIcon.url && user.profileIcon.initials) ?? ''}
     </Container>
   );
-};
-
-ProfileIcon.defaultProps = {
-  size: 28,
 };
 
 export default ProfileIcon;
